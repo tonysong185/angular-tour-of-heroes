a0b7320d76e8de74d6ff2d36fbd6e9d52635dfa9
TonySongWD <tonysong072@gmail.com>
2017-07-24 16:40:36 +0800

    Showing correctly.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,16 +8,7 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `,
+  templateUrl: './hero-detail.component.html',
   styleUrls: [ './hero-detail.component.css' ]
 })
 
