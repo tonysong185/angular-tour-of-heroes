938edaa4e857a028ecf31b566be5d48e5e22cf67
TonySongWD <tonysong072@gmail.com>
2017-07-27 16:59:17 +0800

    update code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule }     from './app-routing.module';
 import { CoreModule } from './core/core.module';
